65a70a2c1eb12a6a751ad915c9f92085ccc35381
Karkachevich <karkachevichtv@yandex.ru>
2023-05-24 12:20:02 +0500

    added class Training

---
+++ training.py
@@ -0,0 +1,37 @@
+from message import InfoMessage
+
+
+class Training:
+    """Базовый класс тренировки."""
+    H_IN_M: int = 60
+    M_IN_KM: int = 1000
+    LEN_STEP: float = 0.65
+
+    def __init__(self,
+                 action: int,
+                 duration: float,
+                 weight: float,
+                 ) -> None:
+        self.action = action  # количество совершённых действий
+        self.duration = duration  # длительность тренировки
+        self.weight = weight  # вес спортсмена
+
+    def get_distance(self) -> float:
+        """Получить дистанцию в км."""
+        return self.action * self.LEN_STEP / self.M_IN_KM
+
+    def get_mean_speed(self) -> float:
+        """Получить среднюю скорость движения."""
+        return self.get_distance() / self.duration
+
+    def get_spent_calories(self) -> float:
+        """Получить количество затраченных калорий."""
+        raise NotImplementedError
+
+    def show_training_info(self) -> InfoMessage:
+        """Вернуть информационное сообщение о выполненной тренировке."""
+        return InfoMessage(training_type=self.__class__.__name__,
+                           duration=self.duration,
+                           distance=self.get_distance(),
+                           speed=self.get_mean_speed(),
+                           calories=self.get_spent_calories())
