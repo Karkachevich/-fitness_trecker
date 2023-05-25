21770ecbcb3e0490178eacd88901287ab8cc0b88
Karkachevich <karkachevichtv@yandex.ru>
2023-05-25 13:14:36 +0500

    added dataclass for SportsWalking

--- src/walking.py
+++ src/walking.py
@@ -1,26 +1,25 @@
+from dataclasses import dataclass
+from typing import ClassVar
+
 from training import Training
 
 
+@dataclass
 class SportsWalking(Training):
     """Класс тренировки: Спортивная ходьба."""
-    CALORIES_MEAN_SPEED_MULTIPLIER: int = 0.035
-    CALORIES_MEAN_SPEED_SHIFT: int = 0.029
-    M_IN_SM: int = 100  # Константа для перевода из метров в сантиметры
-    KMH_IN_MS: int = 0.278  # Константа для перевода из км/ч в м/с
-    K_1 = 2  # Константа для перевода в степень.
+    CALORIES_MEAN_SPEED_MULTIPLIER: ClassVar[int] = 0.035
+    CALORIES_MEAN_SPEED_SHIFT: ClassVar[int] = 0.029
+    M_IN_SM: ClassVar[int] = 100  # Константа перевода из метров в сантиметры
+    KMH_IN_MS: ClassVar[int] = 0.278  # Константа для перевода из км/ч в м/с
+    K_1: ClassVar[int] = 2  # Константа для перевода в степень.
 
-    def __init__(self,
-                 action: int,
-                 duration: float,
-                 weight: float,
-                 height: float):
-        super().__init__(action, duration, weight)
-        self.height = height / self.M_IN_SM
+    height: float
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
         speed_mc = self.get_mean_speed() * self.KMH_IN_MS
+        height_cm = self.height / self.M_IN_SM
         return ((self.CALORIES_MEAN_SPEED_MULTIPLIER * self.weight
-                 + (speed_mc ** self.K_1 / self.height)
+                 + (speed_mc ** self.K_1 / height_cm)
                  * self.CALORIES_MEAN_SPEED_SHIFT * self.weight)
                 * (self.duration * self.H_IN_M))
